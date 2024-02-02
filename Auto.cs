4e40391ef66a67b6ab76a665b3107a8301f6fa30
Nina Maier <ninamaier109@gmail.com>
2024-02-02 12:57:33 +0100

    Projektdateien hinzufügen.

---
+++ Auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmlAuto;
+
+public class Auto
+{
+    string _farbe;
+    int _ps;
+    string _marke;
+
+    public Auto(int ps, string marke, string farbe)
+    {
+        this._ps = ps;
+        this._marke = marke;    
+        this._farbe = farbe;
+    }
+
+    public Auto(int ps, string marke)
+    {
+        this._ps = ps;
+        this._marke = marke;
+        this._farbe = "weiß";
+    }
+
+    public string GetFarbe()
+    {
+        return _farbe;
+    }
+
+    public int GetPs()
+    { 
+        return _ps;
+    }
+
+    public string GetMarke()
+    {  
+        return _marke;
+    
+    }
+    public void Fahre()
+    {
+        //auch möglich:
+        //Console.WriteLine("Das Auto mit der Farbe {0} fährt:", _farbe());
+        //auch möglich (mit Eigenschaft):
+        //Console.WriteLine("Das Auto mit der Farbe {0} fährt:", Farbe());
+
+        Console.WriteLine("Das Auto mit der Farbe {0} fährt:", GetFarbe());
+        Console.WriteLine("Brum brum brum...");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using UmlAuto;
+
+Auto a1 = new Auto(120, "Opel", "silber");
+Auto a2 = new Auto(120, "Opel");
+
+// funktioniert nicht, da kein Konstrukor ohne Parameter vorhanden ist
+// Auto a3 = new Aut0();
+
+a1.Fahre();
+a2.Fahre();
